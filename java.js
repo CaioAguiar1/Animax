f8d64754912d000392d5388ec2c2fef1db68ea2c
Caio Aguiar <88971985+CaioAguiar1@users.noreply.github.com>
2023-07-16 19:15:45 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.imagens').src = anything;
+}
+
+function changeCircles(color){
+   const circle = document.querySelector ('.circle');
+   circle.style.background = color;
+}
